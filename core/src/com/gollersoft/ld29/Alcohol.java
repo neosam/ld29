4d5f2c3e1ad0ded647f37173f59d24480435b9e3
Simon Goller <neosam@gmail.com>
2014-04-26 16:58:13 +0200

    Created Alcohol Living object.

---
+++ core/src/com/gollersoft/ld29/Alcohol.java
@@ -0,0 +1,12 @@
+package com.gollersoft.ld29;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by neosam on 26.04.14.
+ */
+public class Alcohol extends Living {
+    public Alcohol(World world) {
+        super(world);
+    }
+}
