da68cb127a23f23b8e31befd77df85aa4da8a517
Simon Goller <neosam@gmail.com>
2014-04-26 10:33:44 +0200

    initial commit

---
+++ desktop/src/com/gollersoft/ld29/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gollersoft.ld29.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gollersoft.ld29.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
